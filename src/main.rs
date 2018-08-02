affc4dabe2626930633c0f43b91dd8ccaf18c409
JolliestJames <martinja@oregonstate.edu>
2018-08-01 20:05:34 -0700

    Add functions for handling events and updating the game state

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,29 @@ use amethyst::renderer::{DisplayConfig, DrawFlat, Event, KeyboardInput,
                          VirtualKeyCode, WindowEvent};
 
 struct Pong;
+
+impl<'a, 'b> State<GameData<'a, 'b>> for Pong {
+    
+    fn handle_event(&mut self, _: StateData<GameData>, event:Event) -> Trans<GameData<'a, 'b>> {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => Trans::Quit.
+                _ => Trans::None,
+            },
+            _ => Trans::None,
+        }
+    }
+
+}
+
+fn update(&mut self, game_state: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+    game_state.data.update(&game_state.world);
+    Trans::None
+}
