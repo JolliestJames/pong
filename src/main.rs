4a5cf2a8f5c2ffe78a90339d0f647f34f5fdc590
JolliestJames <martinja@oregonstate.edu>
2018-08-02 00:36:38 -0700

    Update display config, add run and main functions, move update inside of Pong implmentation

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Pong;
 
 impl<'a, 'b> State<GameData<'a, 'b>> for Pong {
     
-    fn handle_event(&mut self, _: StateData<GameData>, event:Event) -> Trans<GameData<'a, 'b>> {
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::KeyboardInput {
@@ -20,16 +20,42 @@ impl<'a, 'b> State<GameData<'a, 'b>> for Pong {
                             ..
                         },
                     ..
-                } => Trans::Quit.
+                } => Trans::Quit,
                 _ => Trans::None,
             },
             _ => Trans::None,
         }
     }
 
+    fn update(&mut self, game_state: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        game_state.data.update(&game_state.world);
+        Trans::None
+    }
+
 }
 
-fn update(&mut self, game_state: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
-    game_state.data.update(&game_state.world);
-    Trans::None
+fn run() -> Result<()> {
+    let path = "./resources/display_config.ron";
+
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosTex>::new()),
+    );
+
+    let game_data = GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        println!("Error occurred during game execution: {}", e);
+        ::std::process::exit(1);
+    }
 }
